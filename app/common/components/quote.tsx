c083671eba5fe0284b328d5b7131075e66d952c1
Xavier Ladores <xvrlad@xvrlad-MBP.local>
2022-07-31 14:15:30 +1200

    Figured out how to get quotes from API

--- app/common/components/quote.tsx
+++ app/common/components/quote.tsx
@@ -1,8 +1,43 @@
 import { ScriptProps } from "next/script";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import axios from "axios";
 
-const quote: React.FC<ScriptProps> = ({ children }) => {
-  return <div>quote</div>;
+const Quote: React.FC<ScriptProps> = ({ children }) => {
+  const [currentQuote, setCurrentQuote] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://quote-garden.herokuapp.com/api/v3/quotes", {
+        params: {
+          author: "Bill Gates",
+        },
+      })
+      .then((response) => {
+        // console.log(response.data.data[0].quoteText);
+        console.log(response.data.data);
+        // setCurrentQuote(response.data.quoteText);
+      });
+  }, []);
+
+  // useEffect(() => {
+  //   axios
+  //     .get("https://quote-garden.herokuapp.com/api/v3/quotes", {
+  //       params: {
+  //         author: "Bill Gates",
+  //       },
+  //     })
+  //     .then((response) => {
+  //       console.log(response.then());
+  //     });
+  // }, []);
+
+  //   if (currentQuote === null) {
+  //     return <h1>Loading...</h1>;
+  //   } else if (currentQuote !== null) {
+  //     return <div>{currentQuote}</div>;
+  //   }
+  // getQuote;
+  return <div>{currentQuote}</div>;
 };
 
-export default quote;
+export default Quote;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import { Box, Flex } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import DefaultLayout from "../app/common/components/layout/DefaultLayout";
+import Quote from "../app/common/components/quote";
 
 const Home: NextPage = () => {
-  return <DefaultLayout>Hello World</DefaultLayout>;
+  return (
+    <DefaultLayout>
+      <Quote />
+    </DefaultLayout>
+  );
 };
 
 export default Home;
