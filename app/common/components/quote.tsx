1b4a03ee6817ab7a6e21d4ad48478cf9be85e819
xvrlad <69370722+xvrlad@users.noreply.github.com>
2022-08-04 23:55:46 +1200

    Now shows random quotes as intended

--- app/common/components/quote.tsx
+++ app/common/components/quote.tsx
@@ -1,43 +1,31 @@
 import { ScriptProps } from "next/script";
 import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
+import { Text } from "@chakra-ui/react";
 
 const Quote: React.FC<ScriptProps> = ({ children }) => {
   const [currentQuote, setCurrentQuote] = useState(null);
 
   useEffect(() => {
     axios
-      .get("https://quote-garden.herokuapp.com/api/v3/quotes", {
-        params: {
-          author: "Bill Gates",
-        },
+      .get("https://quote-garden.herokuapp.com/api/v3/quotes/random", {
+        // params: {
+        //   author: "Bill Gates",
+        // },
       })
       .then((response) => {
         // console.log(response.data.data[0].quoteText);
-        console.log(response.data.data);
-        // setCurrentQuote(response.data.quoteText);
+        // console.log(response.data.data);
+        // console.log(response.data.data[0].quoteText);
+        setCurrentQuote(response.data.data[0].quoteText);
       });
   }, []);
 
-  // useEffect(() => {
-  //   axios
-  //     .get("https://quote-garden.herokuapp.com/api/v3/quotes", {
-  //       params: {
-  //         author: "Bill Gates",
-  //       },
-  //     })
-  //     .then((response) => {
-  //       console.log(response.then());
-  //     });
-  // }, []);
-
-  //   if (currentQuote === null) {
-  //     return <h1>Loading...</h1>;
-  //   } else if (currentQuote !== null) {
-  //     return <div>{currentQuote}</div>;
-  //   }
-  // getQuote;
-  return <div>{currentQuote}</div>;
+  if (!currentQuote) {
+    return <Text fontSize="3xl">Loading Quote...</Text>;
+  }
+  
+  return <Text fontSize="3xl">{currentQuote}</Text>;
 };
 
 export default Quote;
